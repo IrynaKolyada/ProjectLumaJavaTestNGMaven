7225b02e19cf0a33791ec1ae3e18a500074f1420
IrynaKolyada <iryna.kolyada.us@gmail.com>
2024-06-08 15:12:30 -0600

    refuctor build file

--- src/test/java/com/lumatest/base/BaseTest.java
+++ src/test/java/com/lumatest/base/BaseTest.java
@@ -4,10 +4,6 @@ import com.lumatest.utils.DriverUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Reporter;
 import org.testng.annotations.*;
 
@@ -19,11 +15,12 @@ public abstract class BaseTest {
 
 
     @BeforeSuite
-    protected void setupDriverManager( ) {
-        WebDriverManager.chromedriver().setup();
-        WebDriverManager.firefoxdriver().setup();
+     protected void setupDriverManager() {
+         WebDriverManager.chromedriver().setup();
 
-    }
+         WebDriverManager.firefoxdriver().setup();
+
+     }
 
 
     @Parameters("browser")
