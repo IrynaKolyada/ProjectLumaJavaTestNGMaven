658a7bce77aae20dcfa71794d098cfa932f7a8db
IrynaKolyada <iryna.kolyada.us@gmail.com>
2024-06-10 14:36:39 -0600

    add testng file (#9)
    * add testng file

--- src/test/java/com/lumatest/utils/DriverUtils.java
+++ src/test/java/com/lumatest/utils/DriverUtils.java
@@ -3,27 +3,46 @@ package com.lumatest.utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chromium.ChromiumOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
 
 import java.util.Map;
 
 public class DriverUtils {
-   private static final ChromeOptions chromeOptions;
+    private static final ChromeOptions chromeOptions;
+    private static final FirefoxOptions firefoxOptions;
+    private static final ChromiumOptions<ChromeOptions> chromiumOptions;
 
-   static {
-       chromeOptions = new ChromeOptions();
+    static {
+        chromeOptions = new ChromeOptions();
 
-       chromeOptions.addArguments("--incognito");
-       chromeOptions.addArguments("--headless");
-       chromeOptions.addArguments("--window-size=1920,1080");
-       chromeOptions.addArguments("--disable-gpu");
-       chromeOptions.addArguments("--no-sandbox");
-       chromeOptions.addArguments("--disable-dev-shm-usage");
-       chromeOptions.addArguments("--disable-web-security");
-       chromeOptions.addArguments("--allow-running-insecure-content");
-       chromeOptions.addArguments("--ignore-certificate-errors");
-   }
+        chromeOptions.addArguments("--incognito");
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--window-size=1920,1080");
+        chromeOptions.addArguments("--disable-gpu");
+        chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--disable-dev-shm-usage");
+        chromeOptions.addArguments("--disable-web-security");
+        chromeOptions.addArguments("--allow-running-insecure-content");
+        chromeOptions.addArguments("--ignore-certificate-errors");
 
-    public static WebDriver createChromeDriver(WebDriver driver) {
+        firefoxOptions = new FirefoxOptions();
+        firefoxOptions.addArguments("--incognito");
+        firefoxOptions.addArguments("--headless");
+        firefoxOptions.addArguments("--window-size=1920,1080");
+        firefoxOptions.addArguments("--disable-gpu");
+        firefoxOptions.addArguments("--no-sandbox");
+        firefoxOptions.addArguments("--disable-dev-shm-usage");
+        firefoxOptions.addArguments("--disable-web-security");
+        firefoxOptions.addArguments("--allow-running-insecure-content");
+        firefoxOptions.addArguments("--ignore-certificate-errors");
+
+        chromiumOptions = chromeOptions;
+    }
+
+    private static WebDriver createChromeDriver(WebDriver driver) {
         if (driver != null) {
             driver.quit();
         }
@@ -36,5 +55,45 @@ public class DriverUtils {
         return chromeDriver;
     }
 
+    private static WebDriver createFireFoxDRiver(WebDriver driver) {
+        if (driver != null) {
+            driver.quit();
+        }
+
+        return new FirefoxDriver(firefoxOptions);
+
+    }
+
+    private static WebDriver createChromiumDriver(WebDriver driver) {
+        if (driver != null) {
+            driver.quit();
+        }
+        ChromeDriver chromeDriver = new ChromeDriver((ChromeOptions) chromiumOptions);
+        chromeDriver.executeCdpCommand("Network.enable", Map.of());
+        chromeDriver.executeCdpCommand(
+                "Network.setExtraHTTPHeaders", Map.of("headers", Map.of("accept-language", "en-US,en;q=0.9"))
+        );
+
+        return chromeDriver ;
+    }
+
+    public static WebDriver createDriver(String browser, WebDriver driver) {
+        switch (browser) {
+            case "chrome" -> {
+                return createChromeDriver(driver);
+            }
+            case "firefox" -> {
+                return createFireFoxDRiver(driver);
+            }
+            case "chromium" -> {
+                return createChromiumDriver(driver);
+            }
+            default -> {
+
+                return null;
+            }
+        }
+    }
 
 }
+
